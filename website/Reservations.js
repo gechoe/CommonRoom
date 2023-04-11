3b54057f401e20553e246105e5e0529f12b0be55
Ariel Shultz <106987539+ArielShultz@users.noreply.github.com>
2023-04-11 13:06:47 -0400

    Add files via upload

---
+++ website/Reservations.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+// the host:port must match the location where you are running MongoDB
+// the "myDatabase" part can be anything you like
+mongoose.connect('mongodb://127.0.0.1:27017/myDatabase'); 
+
+var Schema = mongoose.Schema;
+
+var reserveSchema = new Schema({
+   roomName: { type: String, required: true, unique: true },
+   dorm: { type: String, required: true, unique: false },
+   floor: { type: Number, required: true, unique: false },
+   time: { type: String, required: true, unique: false }
+});
+
+// export userSchema as a class called Users
+module.exports = mongoose.model('Reservations', reserveSchema);
